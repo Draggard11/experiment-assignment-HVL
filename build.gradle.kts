3e82bf5a8662e3f779d008484da40c0d91e7ef6b
draggard11 <draggard2003@gmail.com>
2025-09-03 10:34:59 +0200

    task 1 done

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.5"
+	id("io.spring.dependency-management") version "1.1.7"
+    id("io.freefair.lombok") version "8.14.2"
+}
+
+group = "hvl.no"
+version = "0.0.1-SNAPSHOT"
+description = "ExerciseHVL"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/java/hvl/no/exercisehvl/ExerciseHvlApplicationTests.java
@@ -0,0 +1,13 @@
+package hvl.no.exercisehvl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseHvlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
