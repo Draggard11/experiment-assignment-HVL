6a85b9f9b53bb0e680d8611386bba3e6827fc70a
dragg <draggard2003@gmail.com>
2025-10-03 17:20:00 +0200

    did task5

--- src/main/java/no/hvl/exercisehvl/ExerciseHvlApplication.java
+++ src/main/java/no/hvl/exercisehvl/ExerciseHvlApplication.java
@@ -4,13 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import redis.clients.jedis.UnifiedJedis;
 
 @SpringBootApplication
 public class ExerciseHvlApplication {
 
 	public static void main(String[] args) {
+        SpringApplication.run(ExerciseHvlApplication.class, args);
+        UnifiedJedis jedis = new UnifiedJedis("redis://localhost:6379");
+        // instead of using persitence or h2database
+        // we can use jedis hashes to retrieve votes on a poll and increment their votecount
 
-		SpringApplication.run(ExerciseHvlApplication.class, args);
+        // for the key it is poll:("its id"):votes
+        // for the value it has the id for the options with is votes being the value
+        // this way the user can send in the pollid and the optionsid and increment it by an amount
+        jedis.hset("poll:id:votes", "optionsid", String.valueOf(10));
+        jedis.hgetAll("poll:id:votes");
+        jedis.hincrBy("poll:id:votes", "optionsid", 1);
+
+        jedis.close();
 	}
 
     @GetMapping("/")
